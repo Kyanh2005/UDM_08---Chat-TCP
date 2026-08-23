31f8492b04c72095437d0f4d39ee2c3c85f5dad9
GiaBao156 <doanbao690@gmail.com>
2026-08-23 19:16:23 +0700

    Mem 5 - Giai doan 1: Tao UserControl rong cho Chat Box

---
+++ Code/ChatApp.Client/UI/ChatBoxUserControl.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows.Forms;
+
+namespace ChatApp.Client.UI
+{
+    /// <summary>
+    /// UserControl để hiển thị khung chat text cơ bản
+    /// Member 5 - Giai đoạn 1: Dựng khung rỗng
+    /// </summary>
+    public partial class ChatBoxUserControl : UserControl
+    {
+        public ChatBoxUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
